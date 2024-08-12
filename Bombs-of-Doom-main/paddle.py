9ed4572a0463b31a6a869c57ee1b8207c3edc405
doa29 <159960133+doa29@users.noreply.github.com>
2024-08-12 18:51:30 -0400

    paddle.py

--- Bombs-of-Doom-main/paddle.py
+++ Bombs-of-Doom-main/paddle.py
@@ -1,4 +1,4 @@
-#Name: Nana Kwasi Owusu
+#Name: Daniel Amayaenvbo, Nana Kwasi Owusu
 #Drexel id: no329
 #Purpose of file: This file defines the Paddle class, a subclass of Drawable and is used to represent the paddle in the game
 from drawable import Drawable
